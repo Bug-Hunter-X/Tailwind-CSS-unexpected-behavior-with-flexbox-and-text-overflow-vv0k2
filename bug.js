94af1a0618de0cf5f37bf36c1f7cce8555eea66b
BugHunter-X <askzenai@gmail.com>
2025-01-27 08:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Example usage of Tailwind CSS classes that might cause unexpected behavior
+<div class="flex items-center justify-center">
+  <div class="bg-red-500 p-4 rounded-lg">
+    <p class="text-white font-bold text-xl">This is some text</p>
+  </div>
+</div>
+```
